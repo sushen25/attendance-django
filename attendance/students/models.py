00518fde1429381255c8f083ec15b81093bc7985
Sushen Satturu <sushen.satturu@wspeonline.com>
2021-02-06 20:20:20 +1100

    Create Student model

--- attendance/students/models.py
+++ attendance/students/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=64, blank=False)
+    last_name = models.CharField(max_length=64, blank=False)
+    phone = models.CharField(max_length=64, blank=False, null=True)
+    year_level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)], blank=False, null=True)
+    school = models.CharField(max_length=64, blank=True, null=True)
+    parent_name = models.CharField(max_length=64, blank=True, null=True)
+
